cc12559e643613c75b7ff7f90921eeacbae3c2e6
Izack Angelo <51211048+izackangelo07@users.noreply.github.com>
2025-10-17 12:12:44 -0300

    Update bot_moto.py

--- bot_moto.py
+++ bot_moto.py
@@ -160,8 +160,8 @@ def generate_report():
     else:
         msg += "Nenhum registro\n"
 
-    msg += f"💰 *GASTO MENSAL* ({nome_mes})\nTotal: R$ {total_mes:.2f}\n\n"
-    msg += f"💰 *GASTO TOTAL*\nTotal: R$ {total_geral:.2f}\n\n"
+    msg += f"💰 *GASTO MENSAL\n* ({nome_mes})\nTotal: R$ {total_mes:.2f}\n\n"
+    msg += f"💰 *GASTO TOTAL*\nTotal: R$ {total_geral:.2f}\n"
     
     # Manutenções
     msg += "\n🧰 *Manutenções:*\n"
